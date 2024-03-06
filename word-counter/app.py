33635751164731b7dbefa345efd7ff69497e094d
spykelionel <social.lionel@gmail.com>
2024-03-06 04:16:01 +0100

    update solution for task 1

--- word-counter/app.py
+++ word-counter/app.py
@@ -1,5 +1,19 @@
-# import modules
+import os
+def count_words_chars_lines(file_name):
+    try:
+        file_path = os.path.join(os.path.dirname(__file__), file_name)
+        with open(file_path, 'r') as file:
+            text = file.read()
+            words = text.split()
+            characters = len(text)
+            lines = text.count('\n') + 1  # Add 1 for the last line
 
-# Read text file
+            print("Number of words:", len(words))
+            print("Number of characters:", characters)
+            print("Number of lines:", lines)
 
-# ...rest
+    except FileNotFoundError:
+        print("File not found.")
+
+count_words_chars_lines("lorem.txt")
+
